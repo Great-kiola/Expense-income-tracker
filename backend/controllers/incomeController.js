3e2db81c24e3678ed8082e7021cdbc678385b660
Ola <hollabodey.kilaso@gmail.com>
2025-05-26 22:11:06 +0100

    feat: enhance incomeController with error handling and Excel download functionality

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const Income = require("../models/Income");
+const xlsx = require("xlsx");
 
 // Add Income source
 exports.addIncome = async (req, res) => {
@@ -8,8 +9,8 @@ exports.addIncome = async (req, res) => {
     try {
         const { icon, source, amount, date } = req.body;
 
-        if (!source || !amount || !date){
-            return res.status(400).json({message: "All fields are required!"});
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required!" });
         }
 
         const newIncome = new Income({
@@ -24,17 +25,51 @@ exports.addIncome = async (req, res) => {
         res.status(200).json(newIncome);
 
     } catch (error) {
-        res.status(500).json({message: "Server Error"});
+        res.status(500).json({ message: "Server Error" });
     }
 }
 
 // Get all income source
 exports.getAllIncome = async (req, res) => {
-    
+    const userId = req.user.id
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.json(income);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
 }
 
 // Delete Income source
-exports.deleteIncome = async (req, res) => {}
+exports.deleteIncome = async (req, res) => { 
+
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({ message: "Income deleted Successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
 
 // Download Excel
-exports.downloadIncomeExcel = async (req, res) => {}
+exports.downloadIncomeExcel = async (req, res) => { 
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({date: -1});
+
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet(data);
+        xlsx.utils.book_append_sheet(wb, ws, "")
+    } catch (error) {
+
+    }
+}
