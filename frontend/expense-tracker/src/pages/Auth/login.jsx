95554e38d7598bad7769a5d407c6d0d347b5d900
Ola <hollabodey.kilaso@gmail.com>
2025-05-29 13:19:36 +0100

    feat: implement login functionality with API integration and axios instance

--- frontend/expense-tracker/src/pages/Auth/login.jsx
+++ frontend/expense-tracker/src/pages/Auth/login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from '../../components/layouts/AuthLayout'
 import { useNavigate, Link } from 'react-router-dom';
 import Input from "../../components/inputs/input"
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const Login = () => {
 
@@ -30,6 +32,27 @@ const Login = () => {
 
 
     // Login API Call
+
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const {token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message){
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+
   }
 
   return (
