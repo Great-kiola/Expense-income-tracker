15f2ce0d8ccc275a61eccab7864c1f82b0404193
Ola <hollabodey.kilaso@gmail.com>
2025-05-30 23:24:47 +0100

    feat: update Navbar component to include side menu toggle functionality and improve layout structure; fix import path for UserContext in Login component

--- frontend/expense-tracker/src/components/layouts/Navbar.jsx
+++ frontend/expense-tracker/src/components/layouts/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
+import SideMenu from './SideMenu';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+const Navbar = ({ activeMenu }) => {
+
+    const [openSideMenu, setOpenSideMenu] = useState(false);
+    return (
+        <div className=''>
+            <button
+                className=''
+                onClick={() => {
+                    setOpenSideMenu(!openSideMenu);
+                }}
+            >
+                {openSideMenu ? (
+                    <HiOutlineX className='' />
+                ) : (
+                    <HiOutlineMenu className='' />
+                )}
+            </button>
+
+            <h2 className=''>Expense Tracker</h2>
+
+
+            {openSideMenu && (
+                <div className=''>
+                    <SideMenu activeMenu={activeMenu} />
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Navbar
