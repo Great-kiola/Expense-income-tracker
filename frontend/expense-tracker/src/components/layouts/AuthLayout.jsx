dd9314fd0359e37e73c8ba361e2c86d7e7baa1f7
Ola <hollabodey.kilaso@gmail.com>
2025-06-06 21:24:41 +0100

    fix: update document title and clean up AuthLayout component structure

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -9,6 +9,7 @@ const AuthLayout = ({ children }) => {
             <h2 className='text-lg font-medium text-black'> Expense-tracker</h2>
             {children}
         </div>
+        
 
 
         <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
@@ -17,7 +18,7 @@ const AuthLayout = ({ children }) => {
             <div className='w-48 h-48 rounded-[40px] border-[20px] border-fuchsia-600 absolute -top-[30px] -right-10' />
             <div className='w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-2' />
 
-            <div className="grid grid-cols-1 z-20">
+            <div className="grid">
                 <StatsInfoCard 
                     icon={<LuTrendingUpDown />}
                     label="Track Your Income & expenses"
@@ -26,16 +27,16 @@ const AuthLayout = ({ children }) => {
                 />
             </div>
 
-            <img
+            {/* <img
                 src={CARD_2}
                 className='w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15'
-            />
+            /> */}
         </div>
     </div>
 
 }
 
-export default AuthLayout;
+export default AuthLayout; 
 const StatsInfoCard = ({icon, label, value, color}) => {
     return <div className='flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10'>
         <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>{icon}</div>
